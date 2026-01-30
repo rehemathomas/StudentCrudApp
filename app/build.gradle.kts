a8817c2d892f16f1a88a54b4db0b8523f02c8558
Rehema Thomas <thomasrey850@gmail.com>
2026-01-30 08:43:48 +0300

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -44,7 +44,7 @@ plugins {
 }
 
 dependencies {
-    // Core Android
+    
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.7.0")
     implementation("androidx.activity:activity-compose:1.9.0")
